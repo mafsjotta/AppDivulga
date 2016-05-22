4773b9ea097ab202ac9e3fc910cbba910c0a0fb8
Mafalda Garcia <mafajotta@gmail.com>
2016-05-22 21:20:30 +0100

    cchdsvbcx ovs

--- Divulga/FirstViewController.swift
+++ Divulga/FirstViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import MapKit
 
 class FirstViewController: UIViewController {
+    
+    var events = [Event]()
 
+    @IBOutlet weak var image2View: UIImageView!
+    
+    @IBOutlet weak var name2Label: UILabel!
+    
+    @IBOutlet weak var date2Label: UILabel!
+    
+    @IBOutlet weak var org2Label: UILabel!
+
+ 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
+        
+            }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    
 }
 

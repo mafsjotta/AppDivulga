59e4dc5d3ad0447c190548d9989b69cc16c16178
José Senart <zecams@gmail.com>
2016-05-23 14:09:55 +0100

    Check box, moreira

---
+++ Divulga/CheckBox.swift
@@ -0,0 +1,46 @@
+//
+//  CheckBox.swift
+//  Divulga
+//
+//  Created by José Senart on 22/05/16.
+//  Copyright © 2016 LID. All rights reserved.
+//
+
+import UIKit
+
+class CheckBox: UIButton {
+    
+    //images
+    let checkedImage = UIImage(named: "checked_checkbox")
+    let unCheckedImage = UIImage(named: "unchecked_checkbox")
+    
+    //bool propety
+    @IBInspectable var isChecked:Bool = false{
+        didSet{
+            self.updateImage()
+        }
+    }
+    
+    
+    override func awakeFromNib() {
+        self.addTarget(self, action: #selector(CheckBox.buttonClicked(_:)), forControlEvents: UIControlEvents.TouchUpInside)
+        self.updateImage()
+    }
+    
+    
+    func updateImage() {
+        if isChecked == true{
+            self.setImage(checkedImage, forState: .Normal)
+        }else{
+            self.setImage(unCheckedImage, forState: .Normal)
+        }
+        
+    }
+    
+    func buttonClicked(sender:UIButton) {
+        if(sender == self){
+            isChecked = !isChecked
+        }
+    }
+    
+}

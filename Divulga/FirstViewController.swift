53dc4bff5028c1832c625c3cf84655aa2c808eca
José Senart <zecams@gmail.com>
2016-05-28 22:27:14 +0100

    g

--- Divulga/FirstViewController.swift
+++ Divulga/FirstViewController.swift
@@ -76,11 +76,39 @@ class FirstViewController: UIViewController {
     }
     
     
+    // abrir no safari
+    
     @IBAction func linkButton(sender: AnyObject) {
         
         let openLink = NSURL(string : "http://amitnkalra.github.io")
         UIApplication.sharedApplication().openURL(openLink!)
     }
+
+    // sacar imagem na net
+    
+    func getDataFromUrl(url:NSURL, completion: ((data: NSData?, response: NSURLResponse?, error: NSError? ) -> Void)) {
+        NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            completion(data: data, response: response, error: error)
+            }.resume()
+    }
+    
+    func downloadImage(url: NSURL){
+        print("Download Started")
+        print("lastPathComponent: " + (url.lastPathComponent ?? ""))
+        getDataFromUrl(url) { (data, response, error)  in
+            dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                guard let data = data where error == nil else { return }
+                print(response?.suggestedFilename ?? "")
+                print("Download Finished")
+                self.imageView.image = UIImage(data: data)
+            }
+        }
+    }
+    
+    
+    
+    
+
     
     
     @IBAction func addEvent(sender: AnyObject) {
